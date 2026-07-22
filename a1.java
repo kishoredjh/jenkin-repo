dce15c1813007f8ff2dd5614e3fd3f09e6d375f3
kishoredjh <kishore280303@gmail.com>
2026-07-22 22:38:09 +0530

    modified a1.java

--- a1.java
+++ a1.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String original = str;
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (original.equalsIgnoreCase(reverse)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
